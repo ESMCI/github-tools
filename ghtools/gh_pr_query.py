b889c705010280b057417a57c1c5560777f57da6
Bill Sacks <sacks@ucar.edu>
2020-04-23 13:19:04 -0600

    Add a --show-comments option
    For now this is mutually exclusive with the --todo option, because it
    seems messy to output both of them in succession. Eventually, I'm
    thinking of adding an option to output to files in a directory rather
    than stdout; in that case, you could provide both options, with each of
    them going to a separate file.

--- ghtools/gh_pr_query.py
+++ ghtools/gh_pr_query.py
@@ -12,21 +12,25 @@ def main():
     args = _commandline_args()
     gh_pr_query(repo=args.repo,
                 pr_number=args.pr_number,
+                show_comments=args.show_comments,
                 todo=args.todo,
                 access_token=args.access_token)
 
-def gh_pr_query(repo, pr_number, todo, access_token=None):
+def gh_pr_query(repo, pr_number, todo, show_comments, access_token=None):
     """Implementation of the gh-pr-query command
 
     Args:
     repo: string - GitHub repository, in the form ORG/REPO
     pr_number: integer - Pull Request number
+    show_comments: boolean - Whether to print all comments from this PR
     todo: boolean - Whether to print all outstanding todo items in this PR
     access_token: string - A GitHub personal access token
     """
     pull_request = fetch_pull_request(repo=repo,
                                       pr_number=pr_number,
                                       access_token=access_token)
+    if show_comments:
+        print(pull_request)
     if todo:
         print_pr_todos(pull_request)
 
@@ -64,8 +68,13 @@ To show all of the outstanding todo items in all comments in a pull request
     parser.add_argument('-p', '--pr-number', required=True, type=int,
                         help='Pull Request number')
 
-    parser.add_argument('-t', '--todo', action='store_true',
-                        help='Print all outstanding todo items in this PR')
+    mode = parser.add_mutually_exclusive_group(required=True)
+
+    mode.add_argument('-s', '--show-comments', action='store_true',
+                      help='Print all comments from this PR')
+
+    mode.add_argument('-t', '--todo', action='store_true',
+                      help='Print all outstanding todo items in this PR')
 
     parser.add_argument('-a', '--access-token',
                         help='GitHub personal access token (like a password)\n'
@@ -74,10 +83,5 @@ To show all of the outstanding todo items in all comments in a pull request
                         'For more information, see:\n'
                         'https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line') # pylint: disable=line-too-long
 
-    # FIXME(wjs, 2020-04-23) Add a -s/--show-comments argument. Ensure that at least one
-    # of --todo or --show-comments is given. Also add an --output-dir optional argument,
-    # and ensure that this is given if both --todo and --show-comments are given. (But
-    # actually, maybe initially, just allow one of the two without the --output-dir
-    # option; can add that as a later feature.)
     args = parser.parse_args()
     return args
