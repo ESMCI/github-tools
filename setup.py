29be94cd34d4c1d49fd2a40c06fcc89d1bcb28fe
Bill Sacks <sacks@ucar.edu>
2020-04-29 17:09:54 -0600

    Add a setup.py to allow pip install
    Following instructions here:
    https://packaging.python.org/tutorials/packaging-projects/ and
    https://python-packaging.readthedocs.io/en/latest/minimal.html

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="esmci_github_tools",
+    version="0.1",
+    author="Bill Sacks",
+    author_email="sacks@ucar.edu",
+    description="Tools for working with GitHub repositories from the command line",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ESMCI/github-tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "PyGithub",
+    ],
+    scripts=[
+        "gh-pr-query",
+    ],
+)
