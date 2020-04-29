5a331fde85fdc89379e12ed273f1256e16f46481
Bill Sacks <sacks@ucar.edu>
2020-04-29 07:31:34 -0600

    Rework filling of multi-paragraph text
    I realized that, with my previous method, lines were being broken more
    than they needed to be: it appears that a single embedded newline was
    not resetting the count of the text wrapper, so (I think) it would break
    lines both at the explicit newline characters and wherever would be
    needed to maintain short lines as if the newline characters were
    something like a space.
    
    I realized that a simpler way to get output like I want is simply to
    split the text into lines and wrap each line separately.
    
    My original method would be good (I think) if I didn't want to maintain
    single embedded newline characters (in which case I could use
    replace_whitespace=True in the text wrapper). But since I do want to
    maintain these single embedded newline characters, this new method is
    simpler and produces better results.

--- ghtools/utils.py
+++ ghtools/utils.py
@@ -2,35 +2,6 @@
 
 import textwrap
 
-def split_paragraphs(text):
-    """Split text into paragraphs
-
-    Returns a list of paragraphs - i.e., lines separated by one or more blank lines
-
-    Args:
-    text: string
-    """
-    text_stripped = text.strip()
-    lines = text_stripped.splitlines()
-
-    # paragraphs will be a list of lists of lines
-    paragraphs = []
-
-    cur_paragraph = []
-    for line in lines:
-        if line.strip():
-            # line has more than spaces: it's part of the current paragraph
-            cur_paragraph.append(line)
-        else:
-            # line is empty - i.e., a paragraph separator
-            if cur_paragraph:
-                paragraphs.append(cur_paragraph)
-            cur_paragraph = []
-    if cur_paragraph:
-        paragraphs.append(cur_paragraph)
-
-    return ['\n'.join(one_paragraph) for one_paragraph in paragraphs]
-
 def fill_multiparagraph(text, width):
     """Fill a string that may contain multiple paragraphs
 
@@ -40,13 +11,12 @@ def fill_multiparagraph(text, width):
     text: string
     width: integer - width for filling
     """
-    text_by_paragraph = split_paragraphs(text)
-    # In the following, we use replace_whitespace=False to maintain line breaks in code
-    # blocks (and any other intentional line breaks). However, this can mean more line
-    # breaks than we'd really want.
-    text_filled = [textwrap.fill(one_paragraph,
+    # Note that we fill each line separately. The main point of this is to maintain line
+    # breaks in code blocks (and any other intentional line breaks). However, this can
+    # mean more line breaks than we'd really want.
+    text_by_line = text.splitlines()
+    text_filled = [textwrap.fill(one_line,
                                  width=width,
-                                 break_long_words=False,
-                                 replace_whitespace=False)
-                   for one_paragraph in text_by_paragraph]
-    return '\n\n'.join(text_filled)
+                                 break_long_words=False)
+                   for one_line in text_by_line]
+    return '\n'.join(text_filled)
